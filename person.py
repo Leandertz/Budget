2351320f69453ba5c620234a9902f33fa5fa8c8a
Martin Leandertz <martin.leandertz@Martin-Leandertz.local>
2020-02-25 16:44:08 +0100

    Update with new input form - # A test commit and push

--- person.py
+++ person.py
@@ -4,19 +4,20 @@ import sys
 
 import requests
 
-name = input("Your name please: ")
+f_name = input("Your first name please: ")
+l_name = input("Your last name please: ")
 
 # print(sys.version)
 # print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello " + who_to_greet
+def greet(f_name, l_name):
+    greeting = f"Hej {f_name}! Jag har hört att du heter {l_name} i efternamn"
     return greeting
 
 
-print(greet(name))
-print(greet("Martin"))
+print(greet(f_name, l_name))
+print("Hej då!")
 
 r = requests.get("http://www.leandertz.se")
 print(r.status_code)
